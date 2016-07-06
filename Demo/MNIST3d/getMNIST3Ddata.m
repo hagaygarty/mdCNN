f0b06eb4eab9b8176a8fbd7716487f0733d4a9b9
hagaygarty <hagaygarty@gmail.com>
2016-07-06 17:47:07 +0300

    minimize mem consumption when building the 3d database

--- Demo/MNIST3d/getMNIST3Ddata.m
+++ Demo/MNIST3d/getMNIST3Ddata.m
@@ -33,11 +33,11 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [res] = Transform2dto3d(image,len)
-    res = zeros([size(image) size(image,1)]);
+    res = zeros([size(image) size(image,1)], 'uint8');
     for z=0:(len-1)
         SE = strel('diamond',len-z);
         deIm = imdilate(image,SE,'same');
-        edgeIm = 255*edge(deIm);
+        edgeIm = uint8(255*edge(deIm));
         res(:,:, round(size(res,3)/2)+z) = edgeIm;
         res(:,:, round(size(res,3)/2)-z) = edgeIm;
     end
