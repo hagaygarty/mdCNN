798b24f7449e9fbe843e2e95799f9b6f26f2a0b8
hagaygarty <hagaygarty@gmail.com>
2018-12-01 11:14:49 +0200

    correct function name

--- Training/CrossEnt_Cost.m
+++ Training/CrossEnt_Cost.m
@@ -1,3 +1,3 @@
-function [ answer ] = CrossEnt_dCost( activation,expectedOut )
+function [ answer ] = CrossEnt_Cost( activation,expectedOut )
 	answer=-sum((expectedOut).*log(max(eps,activation)) + (1-expectedOut).*log(max(eps,1-activation)));
 end
