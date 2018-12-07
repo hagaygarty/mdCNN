f29dd0275ec082c2754d7612729e012ec8b6909c
hagaygarty <hagaygarty@gmail.com>
2018-12-07 21:12:40 +0200

    add assertion on activation values

--- Training/CrossEnt.m
+++ Training/CrossEnt.m
@@ -1,4 +1,5 @@
 function [ answer ] = CrossEnt( activation,expectedOut )
+    assert(~any(activation(:)>1)&&~any(activation(:)<0),'Error when using cross ent, activation must be [0..1], add softmax or change last layer activation');
 	answer=(activation-expectedOut)./max(eps,activation.*(1-activation));%cross entropy
 	
 	nanPos = (activation-expectedOut==0) & (activation.*(1-activation)==0); % eliminate 0/0
