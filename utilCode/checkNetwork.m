6eaaa96e6152343fd2dde814cb4df68d2926f1fc
hagaygarty <hagaygarty@gmail.com>
2018-12-18 19:17:33 +0200

    speedup - test on full batch instead of a single sample

--- utilCode/checkNetwork.m
+++ utilCode/checkNetwork.m
@@ -1,14 +1,4 @@
-function [ confMat4 ] = checkNetwork(nets,num, images , useTest , showMiddlePatchOnly)
-
-  if (~exist('showMiddlePatchOnly','var'))
-     showMiddlePatchOnly=0;
-  end
-
-  
-if ( ~iscell(nets) )
-    tmp{1}=nets;
-    nets=tmp;
-end
+function [ confMat4 ] = checkNetwork(net, numSamples, images , useTest)
 
 rnd=0;
 
@@ -26,132 +16,128 @@ else
     labels=images.labels_test;
 end
 
-if (num==0 || num==Inf)
-    num=length(labels);
+if (numSamples==0 || numSamples==Inf)
+    numSamples=length(labels);
 end
 
 startIter=clock;
-failedCount=0;
-numLabels = nets{1}.properties.numOutputs;
+numLabels = net.properties.numOutputs;
 labelDist=(zeros(1,numLabels));
-failedIndex = [];
-failedMSE = [];
+
 range=1:length(labels);
 
-nets{1}.hyperParam.testOnMiddlePatchOnly=0;
+net.hyperParam.testOnMiddlePatchOnly=0;
 
-if ( ~isfield(nets{1}.hyperParam, 'testNumPatches'))
-    numPatchesToTest =1;
-else
-    numPatchesToTest = nets{1}.hyperParam.testNumPatches;    
-end
 confMat=zeros(numLabels,numLabels);
-confMat2=zeros(numLabels,num);
-confMat3=zeros(numLabels,num);
-fprintf('Testing on %d images...\n',num);
-for idx=1:num;
+confMatTargets=zeros(numLabels,numSamples);
+confMatOutputs=zeros(numLabels,numSamples);
+
+fprintf('Testing on %d samples...\n',numSamples);
+
+batchNum = net.hyperParam.batchNum;
+
+BatchSample=zeros([net.layers{1}.properties.sizeFm net.layers{1}.properties.numFm 0]); % create an empty batch
+expectedOut=zeros([net.layers{end}.properties.sizeOut 0]);% create an empty expected classification
+netClassification = zeros(numSamples,1);
+realClass         = zeros(numSamples,1);
+loss              = zeros(numSamples,1);
+datasetIndexes    = zeros(numSamples,1);
+
+for idx=1:numSamples
     if ( rnd==1 )
-        rndIndx = randi(length(range)); 
+        rndIndx = randi(length(range));
         imIdx = range(rndIndx);
         range(rndIndx) = [];% in order not to pick the same twice
     else
         imIdx = idx;
     end
     
+    if (mod(idx,1000)==0)
+        fprintf('Finished checking %d samples (time: %f)\n',idx, etime(clock ,startIter));
+        startIter=clock;
+    end
     
+    datasetIndexes(idx)=imIdx;
     image=I{imIdx};
     label = labels(imIdx);
     labelDist(label+1) = labelDist(label+1) + 1;
-
-    patchAccumRes=0;
-    for patchIdx=1:numPatchesToTest
-        patch = GetNetworkInputs(image, nets{1}, 1);
-        nets{1} = feedForward(nets{1}, patch , 1);
-        patchAccumRes=patchAccumRes+nets{1}.layers{end}.outs.activation;
-    end
-
-    patchAccumRes = patchAccumRes/numPatchesToTest;
-
-    [M,m] = max(patchAccumRes);
-
-         
-    expectedOut=zeros(nets{1}.layers{end}.properties.numFm,1);
-    expectedOut(label+1)=1;
-
-    res(idx) = (m-1==label);
-    conf(idx) = M*res(idx);
     
-    confMat(label+1 , m ) = confMat(label+1 , m )+1;
-    confMat2(label+1,idx) = 1;
-    confMat3(:,idx) = patchAccumRes/sum(patchAccumRes);
-
-    cost = nets{1}.layers{end}.properties.costFunc(nets{1}.layers{end}.outs.activation,expectedOut);
-    err(idx) = mean(cost(:));
+    batchIdx = 1+mod(idx-1,batchNum);
+    BatchSample(:,:,:,:,batchIdx) =  GetNetworkInputs(image, net, 1);
+    expectedOut(:,:,batchIdx)=zeros(net.layers{end}.properties.sizeOut);
+    expectedOut(1,label+1,end)=1;
     
-    
-    if ( res(idx)==0)
-        failedCount=failedCount+1;
-        failedIndex(failedCount)=imIdx;
-        failedMSE(failedCount)=err(idx);
+    if (batchIdx<batchNum)&&(idx<numSamples)
+        continue;
     end
     
-    if (mod(idx,1000)==0)
-        fprintf('Finished checking %d images (time: %f)\n',idx, etime(clock ,startIter));
-        startIter=clock;
-    end
+    % finished filling the batch or reached last sample, classify the batch
+    idxRange=idx-size(BatchSample,5)+1:idx;
+    
+    net = feedForward(net, BatchSample , 1);
+    
+    
+    [~,netClassification(idxRange)]      = max(squeeze(net.layers{end}.outs.activation));
+    [~,realClass(idxRange)] = max(squeeze(expectedOut));
+    
+    cost = net.layers{end}.properties.costFunc(net.layers{end}.outs.activation,expectedOut);
+    loss(idxRange)              =  squeeze(mean(cost));
+    
+    confMatOutputs(:,idxRange) = net.layers{end}.outs.activation./sum(net.layers{end}.outs.activation,2);
+    
+    BatchSample=zeros([net.layers{1}.properties.sizeFm net.layers{1}.properties.numFm 0]); % clear the batch
+    expectedOut=zeros([net.layers{end}.properties.sizeOut 0]);% create an empty expected classification
+    
+    
 end
 
-successRate = sum(res)/length(res)*100;
+for smplIdx=1:length(realClass)
+    confMat( netClassification(smplIdx), realClass(smplIdx) ) = confMat( netClassification(smplIdx), realClass(smplIdx) )+1;
+    confMatTargets(realClass(smplIdx),smplIdx) = 1;
+end
+
+
+successRate = sum(realClass==netClassification)/length(realClass)*100;
 fprintf('success rate %f%%\n',successRate);
 
 failureHist=(zeros(1,numLabels)); % zero till 9
 
-numFailed = length(res)-sum(res);
+numFailed = sum(realClass~=netClassification);
 
-if (~isempty(failedIndex))
-    figure('Name',['Selected errors - num nets=' num2str(length(nets)) ' success rate ' num2str(successRate) '% , total images ' num2str(length(res)) , ' missed ' num2str(numFailed) ' TestSet=' num2str(useTest)]);
+if (numFailed>0)
+    figure('Name',['Selected errors - num nets=' num2str(length(net)) ' success rate ' num2str(successRate) '% , total images ' num2str(length(realClass)) , ' missed ' num2str(numFailed) ' TestSet=' num2str(useTest)]);
 end
 
 maxImagePerAxe = 5;
-imagePerAxe = min(maxImagePerAxe,round(numFailed^0.5 )+1);    
+imagePerAxe = min(maxImagePerAxe,round(numFailed^0.5 )+1);
 
-[~, worseMSEIdx] = sort(failedMSE,'descend');
-imgCount=0;
-for idx=worseMSEIdx
-    imgCount=imgCount+1;
-    imIdx=failedIndex(idx);
+[~, worseLossIdx] = sort(loss(realClass~=netClassification),'descend');
+for idx=1:length(worseLossIdx)
+    imIdx=datasetIndexes(worseLossIdx(idx));
     label = labels(imIdx);
     failureHist(label+1) = failureHist(label+1)+1;
-
-    if ( imgCount > maxImagePerAxe*maxImagePerAxe)
+    
+    if ( idx > maxImagePerAxe*maxImagePerAxe)
         continue
     end
-
+    
     image=double(I{imIdx});
     
-    patchAccumRes=0;
-    for patchIdx=1:numPatchesToTest
-        patch = GetNetworkInputs(image, nets{1}, 1);
-        nets{1} = feedForward(nets{1}, patch , 1);
-        patchAccumRes=patchAccumRes+nets{1}.layers{end}.outs.activation;
-    end
-
-    patchAccumRes = patchAccumRes/numPatchesToTest;
-
-    [E, sortedOut] = sort(patchAccumRes); %#ok<ASGLU>
-
-    expectedOut=zeros([nets{1}.layers{end}.properties.sizeOut 1]);
-    expectedOut(1,label+1,1)=1;
-         
-    MSE = sumDim(nets{1}.layers{end}.properties.costFunc(patchAccumRes,expectedOut), 1:length(nets{1}.layers{end}.properties.sizeOut) );
+    net = feedForward(net, GetNetworkInputs(image, net, 1) , 1);
     
-         
-
-    h = subplot(imagePerAxe,imagePerAxe,imgCount);
+    [E, sortedOut] = sort(net.layers{end}.outs.activation); %#ok<ASGLU>
+    
+    expectedOut=zeros([net.layers{end}.properties.sizeOut 1]);
+    expectedOut(1,label+1,1)=1;
+    
+    cost = net.layers{end}.properties.costFunc(net.layers{end}.outs.activation,expectedOut);
+    loss  =  mean(cost(:));
+    
+    h = subplot(imagePerAxe,imagePerAxe,idx);
     axis off
     im = image/max(image(:));
     
-    if ( (nets{1}.layers{1}.properties.numFm==1)&&(sum(nets{1}.layers{1}.properties.sizeFm>1)==3))
+    if ( (net.layers{1}.properties.numFm==1)&&(sum(net.layers{1}.properties.sizeFm>1)==3))
         %3d image
         if ( showMiddlePatchOnly==1)
             imshow(im(:,:,round(size(im,3)/2)),'Border','loose');
@@ -162,19 +148,17 @@ for idx=worseMSEIdx
     else
         imshow(im,'Border','loose');
     end
-    title({[ 'Idx=' num2str(imIdx) ' MSE=' num2str(MSE)],['Ret=' num2str(sortedOut(end)-1) ',' num2str(sortedOut(end-1)-1) ' real=' num2str(label)]}, 'FontSize', 7); 
+    title({[ 'Idx=' num2str(imIdx) ' loss=' num2str(loss)],['Ret=' num2str(sortedOut(end)-1) ',' num2str(sortedOut(end-1)-1) ' real=' num2str(label)]}, 'FontSize', 7);
 end
 
-if (~isempty(failedIndex))
-    figure('Name','Success rate per label');
-    bar(0:(numLabels-1),100-100*failureHist./labelDist);
-    xlabel('Label');
-    ylabel('Success rate %');
+figure('Name','Success rate per label');
+bar(0:(numLabels-1),100-100*failureHist./labelDist);
+xlabel('Label');
+ylabel('Success rate %');
 
-end
 
 figure
-plotconfusion(confMat2,confMat3);
+plotconfusion(confMatTargets,confMatOutputs);
 confMat4=confMat./repmat(sum(confMat,2),1,size(confMat,2));
 figure;
 surf(confMat4);
